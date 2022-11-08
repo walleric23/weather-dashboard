47883db021194d7b7164b6d2f5ccfa121b1d558d
walleric23 <walleric23@gmail.com>
2022-11-07 17:02:02 -0800

    finished HTML + JavaScript

--- script.js
+++ script.js
@@ -26,8 +26,52 @@ document.querySelector(".search-Btn").addEventListener("click", function () {
           for (var i = 7; i < data.list.length; i += 7) {
             var date = document.createElement("p");
             date.innerText = moment.unix(data.list[i].dt).format("MMM Do YY");
-            document.querySelector(".fiveDay").appendChild(date);
           }
+          document.querySelector(".date1").innerText = moment()
+            .add(1, "days")
+            .format("MMM Do YY");
+          document.querySelector(".temp1").innerText =
+            "Temp " + data.list[0].main.temp + "F";
+          document.querySelector(".wind1").innerText =
+            "Wind " + data.list[0].wind.speed + "MPH";
+          document.querySelector(".humidity1").innerText =
+            "Humidity " + data.list[0].main.humidity + "%";
+          document.querySelector(".date2").innerText = moment()
+            .add(2, "days")
+            .format("MMM Do YY");
+          document.querySelector(".temp2").innerText =
+            "Temp " + data.list[7].main.temp + "F";
+          document.querySelector(".wind2").innerText =
+            "Wind " + data.list[7].wind.speed + "MPH";
+          document.querySelector(".humidity2").innerText =
+            "Humidity " + data.list[7].main.humidity + "%";
+          document.querySelector(".date3").innerText = moment()
+            .add(3, "days")
+            .format("MMM Do YY");
+          document.querySelector(".temp3").innerText =
+            "Temp " + data.list[15].main.temp + "F";
+          document.querySelector(".wind3").innerText =
+            "Wind " + data.list[15].wind.speed + "MPH";
+          document.querySelector(".humidity3").innerText =
+            "Humidity " + data.list[15].main.humidity + "%";
+          document.querySelector(".date4").innerText = moment()
+            .add(4, "days")
+            .format("MMM Do YY");
+          document.querySelector(".temp4").innerText =
+            "Temp " + data.list[23].main.temp + "F";
+          document.querySelector(".wind4").innerText =
+            "Wind " + data.list[23].wind.speed + "MPH";
+          document.querySelector(".humidity4").innerText =
+            "Humidity " + data.list[23].main.humidity + "%";
+          document.querySelector(".date5").innerText = moment()
+            .add(5, "days")
+            .format("MMM Do YY");
+          document.querySelector(".temp5").innerText =
+            "Temp " + data.list[31].main.temp + "F";
+          document.querySelector(".wind5").innerText =
+            "Wind " + data.list[31].wind.speed + "MPH";
+          document.querySelector(".humidity5").innerText =
+            "Humidity " + data.list[31].main.humidity + "%";
         });
     });
 });
