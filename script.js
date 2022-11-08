ae7522544631c45a07d85163d0dc0709303f0d70
walleric23 <walleric23@gmail.com>
2022-11-07 21:30:20 -0800

    gitignore and code comments

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
+// event listener for search button on click
 document.querySelector(".search-Btn").addEventListener("click", function () {
   var city = document.querySelector(".search-text").value;
+  // weather api
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=443d8e5cf520868251c1f1b535dc5a64&units=imperial`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      //edits text for city name and time
       document.querySelector(".city-name").innerText = city;
       document.querySelector(".date").innerText = moment
         .unix(data.dt)
+        // date format for moment
         .format("MMM Do YY");
+      // weather icon
       document.querySelector(
         ".icon"
+        // icon source
       ).src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      // displays temperature
       document.querySelector(".temp").innerText =
         "Temp " + data.main.temp + "F";
+      // displays wind speed
       document.querySelector(".wind").innerText =
         "Wind " + data.wind.speed + "MPH";
+      // displays humidity
       document.querySelector(".humidity").innerText =
         "Humidity " + data.main.humidity + "%";
+      // api for 5 day
       var fivedayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=443d8e5cf520868251c1f1b535dc5a64&units=imperial`;
+      // fetches weather info
       fetch(fivedayURL)
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
+          // for loop for every 8 hours out of 40 hours total
           for (var i = 7; i < data.list.length; i += 7) {
             var date = document.createElement("p");
             date.innerText = moment.unix(data.list[i].dt).format("MMM Do YY");
           }
+          // weather info for day 1/5
           document.querySelector(".date1").innerText = moment()
             .add(1, "days")
             .format("MMM Do YY");
@@ -36,6 +49,7 @@ document.querySelector(".search-Btn").addEventListener("click", function () {
             "Wind " + data.list[0].wind.speed + "MPH";
           document.querySelector(".humidity1").innerText =
             "Humidity " + data.list[0].main.humidity + "%";
+          // day 2/5
           document.querySelector(".date2").innerText = moment()
             .add(2, "days")
             .format("MMM Do YY");
@@ -45,6 +59,7 @@ document.querySelector(".search-Btn").addEventListener("click", function () {
             "Wind " + data.list[7].wind.speed + "MPH";
           document.querySelector(".humidity2").innerText =
             "Humidity " + data.list[7].main.humidity + "%";
+          // day 3/5
           document.querySelector(".date3").innerText = moment()
             .add(3, "days")
             .format("MMM Do YY");
@@ -54,6 +69,7 @@ document.querySelector(".search-Btn").addEventListener("click", function () {
             "Wind " + data.list[15].wind.speed + "MPH";
           document.querySelector(".humidity3").innerText =
             "Humidity " + data.list[15].main.humidity + "%";
+          // day 4/5
           document.querySelector(".date4").innerText = moment()
             .add(4, "days")
             .format("MMM Do YY");
@@ -63,6 +79,7 @@ document.querySelector(".search-Btn").addEventListener("click", function () {
             "Wind " + data.list[23].wind.speed + "MPH";
           document.querySelector(".humidity4").innerText =
             "Humidity " + data.list[23].main.humidity + "%";
+          // day 5/5
           document.querySelector(".date5").innerText = moment()
             .add(5, "days")
             .format("MMM Do YY");
